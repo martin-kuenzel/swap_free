df937b42e14b8a67c4f0acf5aa6350551363b543
mlkone <martin.kuenzel@hs-bochum.de>
2019-08-11 15:48:49 +0200

    small changes

--- clear_swap.sh
+++ clear_swap.sh
@@ -6,8 +6,6 @@
 ## calculate difference between swap used and free memory (hint: if swap used is larger than freemem, the system might get unstable on swap deactivation)
 MEM_AFTER_SWAPOFF=$(awk 'BEGIN {LN=0}; $1 ~ /(MemFree|SwapTotal|SwapFree)/ { mem += (LN==0 ? $2 : (LN==1 ? -($2) : $2 ) );  if(LN==2) print mem; LN++; }' /proc/meminfo)
 
-CONT=1
-
 ## if swap used is larger than freemem, we should ask the user to be sure about continuation !
 [ $MEM_AFTER_SWAPOFF -lt 0 ] && { 
 	tput setaf 1 ; 
@@ -15,13 +13,11 @@ CONT=1
 	echo -e "If we continue, the system might get unstable (beyond usability).\n"; 
 	tput sgr0; 
 
-	CONT=0
 	read -p $'Continue anyways ? (Y|n) default: n\n' RE;
 	{ [ "$RE" == "Y" ] && { echo -e '\nOkay we will continue, but you have been warned.\n'; CONT=1; } || { echo "Canceling due to lack of free memory."; exit 1; }; }
 }
 
-
-
 swapoff -a
 sudo swapon -a
 
+exit 0
